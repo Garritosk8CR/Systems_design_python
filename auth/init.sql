a0447dd6922a694c5d27801299010f2afc8ff6d3
Emilio Garro Rangel <emonteror@ucenfotec.ac.cr>
2025-03-29 16:54:13 -0600

    grant privileges and created table USER

--- auth/init.sql
+++ auth/init.sql
@@ -1,1 +1,13 @@
 CREATE USER 'auth_user'@'localhost' IDENTIFIED BY 'Aauth123';
+
+CREATE DATABASE auth;
+
+GRANT ALL PRIVILEGES ON auth.* TO 'auth_user'@'localhost';
+
+USE auth;
+
+CREATE TABLE users (
+    id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
+    email VARCHAR(255) UNIQUE NOT NULL,
+    password VARCHAR(255) NOT NULL
+);
