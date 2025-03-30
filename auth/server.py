1db902c3fc3b08eaf4d6a03ce7bc445350ff4bd4
Emilio Garro Rangel <emonteror@ucenfotec.ac.cr>
2025-03-29 18:25:43 -0600

    login endpoint and method

--- auth/server.py
+++ auth/server.py
@@ -17,3 +17,20 @@ def login():
 
     if not auth or not auth.username or not auth.password:
         return 'Missing credentials', 401
+
+    cursor = mysql.connection.cursor()
+    result = cursor.execute(
+        "SELECT * FROM users WHERE email=%s", (auth.username,)
+    )
+    if result > 0:
+        user = cursor.fetchone()
+        email = user[0]
+        password = user[1]
+
+        if auth.username != email or auth.password != password:
+            return 'Invalid credentials', 401
+        else:
+            return createJWT(auth.username, os.environ.get('JWT_SECRET'), True), 200
+
+    else:
+        return 'Invalid credentials', 401
