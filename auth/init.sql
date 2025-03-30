758eaa70d4eeadee51551c348720452f8ddb6dc1
Emilio Garro Rangel <emonteror@ucenfotec.ac.cr>
2025-03-29 18:08:28 -0600

    created db, table and db user

--- auth/init.sql
+++ auth/init.sql
@@ -11,3 +11,6 @@ CREATE TABLE users (
     email VARCHAR(255) UNIQUE NOT NULL,
     password VARCHAR(255) NOT NULL
 );
+
+INSERT INTO users (email, password) VALUES
+('emilio@email.com', 'Admin123');
